53c54c43b00b2c083d215e44f678fd2b65835e4e
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-04-24 00:35:46 +0800

    Add: backport

---
+++ alasio/base/backport.py
@@ -0,0 +1,56 @@
+from typing import Literal
+
+
+def removeprefix(s, prefix):
+    """
+    Backport `string.removeprefix(prefix)`, which is on Python>=3.9
+
+    Args:
+        s (T):
+        suffix (T):
+
+    Returns:
+        T:
+    """
+    if s.startswith(prefix):
+        return s[len(prefix):]
+    else:
+        return s
+
+
+def removesuffix(s, suffix):
+    """
+    Backport `string.removesuffix(suffix)`, which is on Python>=3.9
+
+    Args:
+        s (T):
+        suffix (T):
+
+    Returns:
+        T:
+    """
+    if s.endswith(suffix):
+        return s[:-len(suffix)]
+    else:
+        return s
+
+
+# Quote LiteralType because typing_extensions.LiteralType is not available on 3.8
+def to_literal(items):
+    """
+    Dynamically create literal object from a list, `Literal[*items]`, which is on Python>=3.11
+
+    Useful when you don't want to write the same things twice like:
+        lang = ['zh', 'en', 'ja', 'kr']
+        langT = Literal['zh', 'en', 'ja', 'kr']
+    With backport you have:
+        lang = ['zh', 'en', 'ja', 'kr']
+        langT = to_literal(lang)
+
+    Args:
+        items (iterable[T]):
+
+    Returns:
+        LiteralType[T]:
+    """
+    return Literal._getitem(Literal, *items)
