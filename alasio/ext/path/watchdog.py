939c43f979bed0433296c23158b6dc01ad42e09b
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-05-04 20:17:28 +0800

    Add: Watchdog to watch file changes

---
+++ alasio/ext/path/watchdog.py
@@ -0,0 +1,201 @@
+import os
+import stat
+import time
+from collections import deque
+
+from alasio.ext.path.calc import normpath
+
+
+def stat_info(st):
+    """
+    Get path information using single os.stat call.
+
+    Args:
+        st (os.stat): Path stat to check
+
+    Returns:
+        Tuple[str, float]: path_type, mtime
+            `path_type` is one of: F for file, D for directory, S for symlink
+
+    Raises:
+        FileNotFoundError:
+    """
+    mode = st.st_mode
+    if stat.S_ISREG(mode):
+        # File
+        path_type = 'F'
+    elif stat.S_ISDIR(mode):
+        # Directory
+        path_type = 'D'
+    elif stat.S_ISLNK(mode):
+        # Symlink
+        path_type = 'S'
+    else:
+        # This should not happen
+        raise OSError
+    return path_type, st.st_mtime
+
+
+class Watchdog:
+    def __init__(self, paths):
+        """
+        Initialize the file monitor with paths to watch.
+
+        Args:
+            paths (list[str] | str): Absolute path or a list of item
+        """
+        if isinstance(paths, list):
+            self.paths = [normpath(p) for p in paths]
+        else:
+            self.paths = [normpath(paths)]
+        self.info_dict = {}
+
+        self._last_snapshot = 0
+        self._first_snapshot = True
+
+    def watch(self, include_existing=False, interval=0):
+        """
+        Args:
+            include_existing (bool): True to return all existing files first
+            interval: Interval in seconds between two _watch calls
+
+        Returns:
+            list[Tuple[str, str, str, float]]:
+                List of change info (event, path, path_type, mtime)
+                `event` is one of: A for added, M for modified, D for deleted.
+                If path renamed, there will be an "A" first then "D"
+                `path_type` is one of: F for file, D for directory, S for symlink
+
+        Examples:
+            self = Watchdog(r'E:\ProgramData\Pycharm\AzurLaneAutoScript')
+            while 1:
+                for row in self.watch(interval=2):
+                    print(row)
+            # ('M', 'E:\\ProgramData\\Pycharm\\AzurLaneAutoScript\\config', 'D', 1746358905.0225341)
+            # ('M', 'E:\\ProgramData\\Pycharm\\AzurLaneAutoScript\\config\\alas.json', 'F', 1746358905.0210333)
+        """
+        if interval > 0:
+            now = time.time()
+            diff = now - self._last_snapshot
+            if 0 <= diff <= interval:
+                time.sleep(interval - diff)
+            self._last_snapshot = time.time()
+
+        result = list(self._watch())
+
+        if self._first_snapshot:
+            self._first_snapshot = False
+            if include_existing:
+                return result
+            else:
+                return []
+        else:
+            return result
+
+    def watch_files(self, include_existing=False, interval=0):
+        """
+        Same as watch() but returns file changes only
+        """
+        result = self.watch(include_existing=include_existing, interval=interval)
+        result = [r for r in result if r[2] == 'F']
+        return result
+
+    def _watch(self):
+        info_dict = self.info_dict
+        new_dict = {}
+        need_visit = deque()
+
+        for path in self.paths:
+            try:
+                info = stat_info(os.stat(path))
+            except OSError:
+                prev_info = info_dict.pop(path, None)
+                if prev_info is None:
+                    # path never exists
+                    continue
+                else:
+                    # path deleted
+                    yield 'D', path, prev_info[0], prev_info[1]
+                    continue
+            try:
+                prev_info = info_dict.pop(path)
+            except KeyError:
+                # new path
+                yield 'A', path, info[0], info[1]
+                new_dict[path] = info
+                if info[0] == 'D':
+                    # new directory
+                    need_visit.append(path)
+                continue
+            new_dict[path] = info
+            # Always scan directories recursively because directory mtime don't get update when sub files changed
+            if info[0] == 'D':
+                need_visit.append(path)
+            if info == prev_info:
+                # same
+                continue
+            else:
+                # path modified
+                yield 'M', path, info[0], info[1]
+                continue
+
+        while True:
+            new_visit = deque()
+            for path in need_visit:
+                try:
+                    list_entry = list(os.scandir(path))
+                except OSError:
+                    # directory got deleted just now
+                    prev_info = info_dict.pop(path, None)
+                    if prev_info is None:
+                        # path never exists
+                        continue
+                    else:
+                        # path deleted
+                        yield 'D', path, prev_info[0], prev_info[1]
+                        continue
+                for entry in list_entry:
+                    entry_path = entry.path
+                    try:
+                        entry_info = stat_info(entry.stat(follow_symlinks=True))
+                    except OSError:
+                        # entry got deleted just now
+                        prev_info = info_dict.pop(entry_path, None)
+                        if prev_info is None:
+                            # path never exists
+                            continue
+                        else:
+                            # path deleted
+                            yield 'D', entry_path, prev_info[0], prev_info[1]
+                            continue
+                    try:
+                        prev_info = info_dict.pop(entry_path)
+                    except KeyError:
+                        # new sub path
+                        yield 'A', entry_path, entry_info[0], entry_info[1]
+                        new_dict[entry_path] = entry_info
+                        if entry_info[0] == 'D':
+                            # new sub directory
+                            new_visit.append(entry_path)
+                        continue
+                    new_dict[entry_path] = entry_info
+                    # Always scan directories recursively because directory mtime don't get update when sub files
+                    # changed
+                    if entry_info[0] == 'D':
+                        new_visit.append(entry_path)
+                    if entry_info == prev_info:
+                        # same
+                        continue
+                    else:
+                        # path modified
+                        yield 'M', entry_path, entry_info[0], entry_info[1]
+                        continue
+            # End
+            need_visit = new_visit
+            if not need_visit:
+                break
+        # path not visited, probably because they are deleted
+        for path, info in info_dict.items():
+            yield 'D', path, info[0], info[1]
+        # swap dict
+        self.info_dict = new_dict
