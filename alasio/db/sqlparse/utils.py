58af230708aefa4707aa0b3f690650bcbe3aa875
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-06-06 03:17:54 +0800

    Add: get_primary_key

--- alasio/db/sqlparse/utils.py
+++ alasio/db/sqlparse/utils.py
@@ -44,3 +44,49 @@ def first_paren_content(string, balance=0):
         return ''
 
     return string[:end_index]
+
+
+def first_token(string):
+    """
+    Get the first token in sql, and handle quotes
+
+    Args:
+        string (str):
+
+    Returns:
+        str:
+    """
+    string = string.lstrip()
+
+    end_index = 0
+    state = State.NORMAL
+    for index, char in enumerate(string):
+        if state == State.NORMAL:
+            if char == "'":
+                state = State.IN_SINGLE_QUOTE_STRING
+            elif char == '"':
+                state = State.IN_DOUBLE_QUOTE_STRING
+            elif char == '`':
+                state = State.IN_BACKTICK_IDENTIFIER
+            elif char in ' \t\r\n':
+                # first space
+                end_index = index
+                break
+
+        elif state == State.IN_SINGLE_QUOTE_STRING:
+            # Single quote ends the string state
+            if char == "'":
+                state = State.NORMAL
+
+        elif state == State.IN_DOUBLE_QUOTE_STRING:
+            # Double quote ends the string/identifier state
+            if char == '"':
+                state = State.NORMAL
+
+        elif state == State.IN_BACKTICK_IDENTIFIER:
+            # Backtick ends the identifier state
+            if char == '`':
+                state = State.NORMAL
+
+    # if no space found string[:0] will still return an empty string
+    return string[:end_index]
