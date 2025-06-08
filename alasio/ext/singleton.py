2a899f2eee63fb5b6df78ac0ef3717ea6b77e2a2
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-06-08 10:22:00 +0800

    Add: Singleton metaclass

---
+++ alasio/ext/singleton.py
@@ -0,0 +1,97 @@
+import threading
+from typing import Type, TypeVar
+
+T = TypeVar('T')
+
+
+class Singleton(type):
+    """
+    A metaclass for creating a global singleton.
+
+    Any class using this metaclass will have only one instance.
+    Subclasses will have their own unique singleton instance.
+    This implementation is thread-safe.
+    """
+    __instances = {}
+    __lock = threading.Lock()
+
+    def __call__(cls: Type[T], *args, **kwargs) -> T:
+        # return cached instance directly
+        try:
+            return cls.__instances['']
+        except KeyError:
+            pass
+
+        # create new instance
+        with cls.__lock:
+            # another thread may have created while we are waiting
+            try:
+                return cls.__instances['']
+            except KeyError:
+                pass
+
+            # create
+            instance = super().__call__(*args, **kwargs)
+            cls.__instances[''] = instance
+            return instance
+
+    def singleton_clear_all(cls):
+        """
+        Remove all instances
+        """
+        with cls.__lock:
+            cls.__instances.clear()
+
+
+class SingletonNamed(type):
+    """
+    A metaclass for creating a named singleton.
+
+    Instances are created based on the first argument provided to the constructor.
+    Each class will have its own separate cache of named instances.
+    This implementation is thread-safe.
+    """
+    __instances = {}
+    __lock = threading.Lock()
+
+    def __call__(cls: Type[T], name, *args, **kwargs) -> T:
+        # return cached instance directly
+        try:
+            return cls.__instances[name]
+        except KeyError:
+            pass
+
+        # create new instance
+        with cls.__lock:
+            # another thread may have created while we are waiting
+            try:
+                return cls.__instances[name]
+            except KeyError:
+                pass
+
+            # create
+            instance = super().__call__(name, *args, **kwargs)
+            cls.__instances[''] = instance
+            return instance
+
+    def singleton_remove(cls, name):
+        """
+        Remove a specific instance.
+        Instance will be re-created, the nest time it is requested.
+
+        Returns:
+            bool: If removed
+        """
+        # delete from dict is threadsafe
+        try:
+            del cls.__instances[name]
+            return True
+        except KeyError:
+            return False
+
+    def singleton_clear_all(cls):
+        """
+        Remove all instances
+        """
+        with cls.__lock:
+            cls.__instances.clear()
