fdf5be6cc67650c67dd0b4c042634884b9c65dad
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-07-07 20:27:59 +0800

    Fix: Handle staging and extended flags

--- alasio/ext/gitpython/stage/index.py
+++ alasio/ext/gitpython/stage/index.py
@@ -21,8 +21,114 @@ class GitIndexEntry(msgspec.Struct):
     gid: int
     size: int
     sha1: bytes
-    flags: int
     path: str
+    # flags
+    # assume file unchanged
+    assume_valid: bool = False
+    # stage during merge
+    stage: int = 0  # 0, 1, 2, or 3
+    # extensions on version >= v3
+    # for sparse checkout
+    skip_worktree: bool = False
+    # for `git add -N``
+    intent_to_add: bool = False
+
+
+# A 16-bit 'flags' field split into (high to low bits)
+# 1-bit assume-valid flag
+# 1-bit extended flag (must be zero in version 2)
+# 2-bit stage (during merge)
+# 12-bit name length if the length is less than 0xFFF; otherwise 0xFFF
+# is stored in this field.
+def _parse_flags(flags):
+    """
+    Args:
+        flags (int): uint16
+
+    Returns:
+        Tuple[bool, bool, int, int]: assume_valid, extended, stage (0~3), path_len (<4096)
+    """
+    if flags >= 32768:
+        assume_valid = True
+        flags -= 32768
+    else:
+        assume_valid = False
+    if flags >= 16384:
+        extended = True
+        flags -= 16384
+    else:
+        extended = False
+    if flags >= 4096:
+        stage = flags // 4096
+        flags %= 4096
+    else:
+        stage = 0
+    return assume_valid, extended, stage, flags
+
+
+def _gen_flags(assume_valid, extended, stage, path_len):
+    """
+    Args:
+        assume_valid (bool):
+        extended (bool):
+        stage (int): 0~3
+        path_len (int): <4096
+
+    Returns:
+        int: uint16
+    """
+    if stage == 1 or stage == 2 or stage == 3:
+        path_len += stage * 4096
+    if extended:
+        path_len += 16384
+    if assume_valid:
+        path_len += 32768
+    return path_len
+
+
+# (Version 3 or later) A 16-bit field, only applicable if the
+# "extended flag" above is 1, split into (high to low bits).
+# 1-bit reserved for future
+# 1-bit skip-worktree flag (used by sparse checkout)
+# 1-bit intent-to-add flag (used by "git add -N")
+# 13-bit unused, must be zero
+def _parse_extended_flag(flags):
+    """
+    Args:
+        flags (int): uint16
+
+    Returns:
+        tuple[bool, bool]: skip_worktree, intent_to_add
+    """
+    if flags >= 32768:
+        flags -= 32768
+    if flags >= 16384:
+        skip_worktree = True
+        flags -= 16384
+    else:
+        skip_worktree = False
+    if flags >= 8192:
+        intent_to_add = True
+    else:
+        intent_to_add = False
+    return skip_worktree, intent_to_add
+
+
+def _gen_extended_flags(skip_worktree, intend_to_add):
+    """
+    Args:
+        skip_worktree (bool):
+        intend_to_add (bool):
+
+    Returns:
+        int: uint16
+    """
+    flags = 0
+    if skip_worktree:
+        flags += 16384
+    if intend_to_add:
+        flags += 8192
+    return flags
 
 
 class GitIndex:
@@ -33,12 +139,14 @@ class GitIndex:
         """
         self.file = file
         # all indexed files in workspace
-        self.list_entry: "list[GitIndexEntry]" = []
+        # key: (filepath, stage), value: GitIndexEntry object
+        # stage is 0 if not in merge
+        self.dict_entry: "dict[tuple[str, int], GitIndexEntry]" = {}
         # git index version, 2, 3, 4, or 0 if not loaded
         self.version = 0
 
     def clear_object(self):
-        self.list_entry = []
+        self.dict_entry = {}
         self.version = 0
 
     def index_read(self, file=None, data=None):
@@ -72,15 +180,14 @@ class GitIndex:
 
         data = memoryview(data)
         if version in [2, 3]:
-            self.list_entry = list(self._parse_v2(data, entry_count))
+            self._parse_v2(data, entry_count)
         elif version in [4]:
-            self.list_entry = list(self._parse_v4(data, entry_count))
+            self._parse_v4(data, entry_count)
         else:
             # we only support v2, v3, v4
             raise GitIndexBroken(f'Unsupported version: {version}')
 
-    @staticmethod
-    def _parse_v2(data, entry_count):
+    def _parse_v2(self, data, entry_count):
         """
         Parse git index file of v2 and v3
 
@@ -91,11 +198,14 @@ class GitIndex:
         Yields:
             GitIndexEntry:
         """
+        dict_entry = {}
         # https://git-scm.com/docs/index-format
         unpacker = struct.Struct('>IIIIIIIIII20sH').unpack
+        unpacker_uint16 = struct.Struct('>H').unpack
         # 12bytes header
         cursor = 12
         total_len = len(data)
+        has_extended_flag = self.version >= 3
 
         for i in range(entry_count):
             # unpack fixed part
@@ -106,10 +216,24 @@ class GitIndex:
                 raise GitIndexBroken(f'File ended prematurely on entry {i} fixed part: '
                                      f'{data[cursor:path_start].hex()}')
             # ctime_s, ctime_ns, mtime_s, mtime_ns, dev, ino, mode, uid, gid, size, sha1, flags = fixed_part
-            flags = fixed_part[11]
+            assume_valid, extended, stage, path_len = _parse_flags(fixed_part[11])
+
+            if extended:
+                if not has_extended_flag:
+                    raise GitIndexBroken(f'Current version {self.version} should not have extended_flag on entry {i}')
+                try:
+                    extended_flag = unpacker_uint16(data[path_start:path_start + 2])[0]
+                except struct.error:
+                    raise GitIndexBroken(f'File ended prematurely on entry {i} has_extended_flag: '
+                                         f'{data[cursor:path_start].hex()}')
+                skip_worktree, intent_to_add = _parse_extended_flag(extended_flag)
+                # move on to path
+                path_start += 2
+            else:
+                skip_worktree = False
+                intent_to_add = False
 
             # calculate path
-            path_len = flags % 4096
             if path_len == 4095:
                 raise GitIndexBroken(f'Filepath length > 4095 bytes on entry {i}')
             if path_len == 0:
@@ -125,14 +249,20 @@ class GitIndex:
                 raise GitIndexBroken(f'Filepath invalid on entry {i}, path={path}, {e}')
 
             # Build info
-            yield GitIndexEntry(*fixed_part, path)
+            dict_entry[path] = GitIndexEntry(
+                *fixed_part[:11], path,
+                assume_valid=assume_valid, stage=stage, skip_worktree=skip_worktree, intent_to_add=intent_to_add)
 
             # calculate next cursor
-            # path is align to 8 bytes
-            cursor += (70 + path_len) // 8 * 8
+            # path is aligned to 8 bytes
+            if extended:
+                cursor += (72 + path_len) // 8 * 8
+            else:
+                cursor += (70 + path_len) // 8 * 8
 
-    @staticmethod
-    def _parse_v4(data, entry_count):
+        self.dict_entry = dict_entry
+
+    def _parse_v4(self, data, entry_count):
         """
         Parse git index file of v4
 
@@ -143,7 +273,9 @@ class GitIndex:
         Yields:
             GitIndexEntry:
         """
+        dict_entry = {}
         unpacker = struct.Struct('>IIIIIIIIII20sH').unpack
+        unpacker_uint16 = struct.Struct('>H').unpack
         # 12bytes header
         cursor = 12
         # v4 reuses previous path
@@ -158,11 +290,23 @@ class GitIndex:
                 raise GitIndexBroken(f'File ended prematurely on entry {i} fixed part: '
                                      f'{data[cursor:path_start].hex()}')
             # ctime_s, ctime_ns, mtime_s, mtime_ns, dev, ino, mode, uid, gid, size, sha1, flags = fixed_part
-            flags = fixed_part[11]
+            assume_valid, extended, stage, path_len = _parse_flags(fixed_part[11])
+
+            if extended:
+                try:
+                    extended_flag = unpacker_uint16(data[path_start:path_start + 2])[0]
+                except struct.error:
+                    raise GitIndexBroken(f'File ended prematurely on entry {i} has_extended_flag: '
+                                         f'{data[cursor:path_start].hex()}')
+                skip_worktree, intent_to_add = _parse_extended_flag(extended_flag)
+                # move on to path
+                path_start += 2
+            else:
+                skip_worktree = False
+                intent_to_add = False
 
             # calculate path
             # path_len is the final length
-            path_len = flags % 4096
             if path_len == 4095:
                 raise GitIndexBroken(f'Filepath length > 4095 bytes on entry {i}')
             if path_len == 0:
@@ -200,11 +344,15 @@ class GitIndex:
                 raise GitIndexBroken(f'Filepath invalid on entry {i}, path={path}, {e}')
 
             # Build info
-            yield GitIndexEntry(*fixed_part, path_str)
+            dict_entry[path] = GitIndexEntry(
+                *fixed_part[:11], path_str,
+                assume_valid=assume_valid, stage=stage, skip_worktree=skip_worktree, intent_to_add=intent_to_add)
 
             # filepath is endswith \x00, skip it
             cursor = path_end + 1
             prev_path = path
+
+        self.dict_entry = dict_entry
 
     def index_write(self, file=None, version=None):
         """
@@ -220,8 +368,6 @@ class GitIndex:
         if version in [2, 3]:
             data = b''.join(self._gen_v2())
         elif version in [4]:
-            for row in self._gen_v4():
-                print(row)
             data = b''.join(self._gen_v4())
         else:
             raise ValueError(f'Unsupported version: {version}')
@@ -233,6 +379,7 @@ class GitIndex:
 
         if file is None:
             file = self.file
+        # self.index_read(data=data)
         atomic_write(file, data)
 
     def _gen_v2(self):
@@ -243,22 +390,24 @@ class GitIndex:
             bytes:
         """
         packer = struct.Struct('>IIIIIIIIII20sH').pack
+        packer_uint16 = struct.Struct('>H').pack
         # Entries must be sorted by path name for the index to be valid
-        self.list_entry.sort(key=lambda e: e.path)
+        list_entry = sorted(self.dict_entry.values(), key=lambda e: (e.path, e.stage))
 
         # header
-        entry_count = len(self.list_entry)
+        entry_count = len(list_entry)
         yield struct.pack('>4sII', b'DIRC', self.version, entry_count)
 
         # content
-        for entry in self.list_entry:
+        for entry in list_entry:
             path = entry.path.encode('utf-8')
             path_len = len(path)
 
             # We assume no extended flags for simplicity here.
             if path_len >= 4095:
                 raise ValueError(f'Path is too long: {entry.path}')
-            flags = path_len
+            extended_flags = _gen_extended_flags(entry.skip_worktree, entry.intent_to_add)
+            flags = _gen_flags(entry.assume_valid, extended_flags > 0, entry.stage, path_len)
 
             # Pack fixed-size metadata and path length
             yield packer(
@@ -266,6 +415,8 @@ class GitIndex:
                 entry.dev, entry.ino, entry.mode, entry.uid, entry.gid, entry.size,
                 entry.sha1, flags
             )
+            if extended_flags:
+                yield packer_uint16(extended_flags)
             yield path
 
             # align to 8 bytes
@@ -282,23 +433,25 @@ class GitIndex:
             bytes:
         """
         packer = struct.Struct('>IIIIIIIIII20sH').pack
+        packer_uint16 = struct.Struct('>H').pack
         # Entries must be sorted by path name for the index to be valid
-        self.list_entry.sort(key=lambda e: e.path)
+        list_entry = sorted(self.dict_entry.values(), key=lambda e: (e.path, e.stage))
 
         # header
-        entry_count = len(self.list_entry)
+        entry_count = len(list_entry)
         yield struct.pack('>4sII', b'DIRC', self.version, entry_count)
 
         # content
         prev_path = b''
-        for entry in self.list_entry:
+        for entry in list_entry:
             path = entry.path.encode('utf-8')
             path_len = len(path)
 
             # We assume no extended flags for simplicity here.
             if path_len >= 4095:
                 raise ValueError(f'Path is too long: {entry.path}')
-            flags = path_len
+            extended_flags = _gen_extended_flags(entry.skip_worktree, entry.intent_to_add)
+            flags = _gen_flags(entry.assume_valid, extended_flags > 0, entry.stage, path_len)
 
             # Pack fixed-size metadata and path length
             yield packer(
@@ -306,6 +459,8 @@ class GitIndex:
                 entry.dev, entry.ino, entry.mode, entry.uid, entry.gid, entry.size,
                 entry.sha1, flags
             )
+            if extended_flags:
+                yield packer_uint16(extended_flags)
 
             # Calculate the shared prefix length with the previous path
             prefix_len = 0
