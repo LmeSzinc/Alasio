d60ea071df5046b6193ece80f2e2535cbc227d0b
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-07-09 12:51:55 +0800

    Add: Mount static files

--- alasio/backend/app.py
+++ alasio/backend/app.py
@@ -1,6 +1,28 @@
+from starlette.middleware.gzip import GZipResponder
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route, WebSocketRoute
+from starlette.staticfiles import StaticFiles
+
+
+class NoCacheStaticFiles(StaticFiles):
+    def file_response(self, *args, **kwargs):
+        resp = super().file_response(*args, **kwargs)
+        # No cache for static files
+        # We've seen too many styling issues in ALAS. We use electron as client and chromium caches static files on
+        # user's disk. Those files may get broke for unknown reason, causing the styling issues.
+        # To fix that, we tell the browsers don't cache any. Bandwidth increase should be acceptable on local service.
+        resp.headers.setdefault('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        resp.headers.setdefault('Expires', '0')
+        resp.headers.setdefault('Pragma', 'no-cache')
+
+        # GZipMiddleware
+        resp = GZipResponder(resp, minimum_size=500, compresslevel=9)
+
+        return resp
+
+
 def create_app():
     from alasio.ext.starapi.router import StarAPI
-
     app = StarAPI()
 
     # All APIs should under /api
@@ -9,11 +31,27 @@ def create_app():
     app.add_router('/api', auth.router)
 
     # Global websocket
-    from starlette.routing import WebSocketRoute
     from alasio.backend.topic import WebsocketServer
     app.routes.append(WebSocketRoute('/api/ws', WebsocketServer.endpoint))
 
-    # Mod APIs
+    # Alasio should be a local service and should not be exposed on public network
+    # We serve in-memory robots.txt to deny all spiders
+    # this router should be added before mounting static files
+    async def robots_txt(request):
+        return PlainTextResponse(content='User-agent: *\nDisallow: /', media_type='text/plain')
+
+    app.routes.append(Route('/robots.txt', robots_txt))
+
+    # Mount static files
+    from alasio.ext.path import PathStr
+    root = PathStr(__file__).uppath(2).joinpath('ext')
+    static_app = NoCacheStaticFiles(directory=root, html=True)
+    app.mount('/', static_app, name='static')
+
+    # Mount mod APIs
+    pass
+
+    # Mount mod static files
     pass
 
     return app
