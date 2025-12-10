48674bc5d28bc826f039c7a1eaddd1daa4aa3bf6
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2025-12-10 23:41:14 +0800

    Fix: Rename to sig to avoid conflict

--- alasio/ext/proc/__init__.py
+++ alasio/ext/proc/__init__.py
@@ -1,4 +1,4 @@
 from .cmd import get_cmdline, get_executable
 from .iter import process_iter
 from .nice import set_lower_process_priority, set_lowest_process_priority
-from .signal import process_kill, process_terminate
+from .sig import process_kill, process_terminate
