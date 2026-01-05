39f82ef2dcc0a1dc148283ff124781cfc9cd2d63
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2026-01-05 22:08:37 +0800

    Perf: use fd directly instead of using print

--- alasio/logger/logger.py
+++ alasio/logger/logger.py
@@ -357,19 +357,23 @@ class AlasioLogger(structlog.BoundLoggerBase):
 class PrintLogger(structlog.PrintLogger):
     def msg(self, text: str, event: dict) -> None:
         writer: LogWriter = self._file
+        text = f'{text}\n'
 
         # do 3 things parallely, print to stdout, write into file, send to backend
         if writer.backend.inited:
             if writer.is_electron:
                 # backend + file
                 job = writer.backend.send_log(event)
-                print(text, file=writer.fd, flush=True)
+                writer.fd.write(text)
+                writer.fd.flush()
                 job.acquire()
             else:
                 # backend + stdout + file
                 job = writer.backend.send_log(event)
-                print(text, file=writer.fd, flush=True)
-                print(text, file=sys.stdout, flush=True)
+                writer.fd.write(text)
+                sys.stdout.write(text)
+                writer.fd.flush()
+                sys.stdout.flush()
                 job.acquire()
         else:
             if writer.is_electron:
@@ -378,8 +382,10 @@ class PrintLogger(structlog.PrintLogger):
                 job.acquire()
             else:
                 # stdout + file
-                print(text, file=writer.fd, flush=True)
-                print(text, file=sys.stdout, flush=True)
+                writer.fd.write(text)
+                sys.stdout.write(text)
+                writer.fd.flush()
+                sys.stdout.flush()
 
 
 structlog.configure(
