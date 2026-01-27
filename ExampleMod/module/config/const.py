2b6dda784d2902dea41a1149a912d5eec0e24469
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2026-01-28 02:49:23 +0800

    Chore: Move generate to const.py

--- ExampleMod/module/config/const.py
+++ ExampleMod/module/config/const.py
@@ -1,5 +1,8 @@
-from alasio.config.entry.const import ModEntryInfo, ConfigConst as ConfigConst_
+from alasio.config.entry.const import ConfigConst as ConfigConst_, ModEntryInfo
+from alasio.config_dev.gen_index import IndexGenerator
+from alasio.ext.env import set_project_root
 from alasio.ext.path import PathStr
+from alasio.logger import logger
 
 entry = ModEntryInfo(name='example_mod')
 entry.root = PathStr.new(__file__).uppath(3)
@@ -28,3 +31,11 @@ class ConfigConst(ConfigConst_):
     > GemsFarming
     > OpsiHazard1Leveling
     """
+
+
+if __name__ == '__main__':
+    set_project_root(__file__, up=4)
+    # generate
+    logger.info(f'ModEntry: {entry}')
+    self = IndexGenerator(entry)
+    self.generate()
